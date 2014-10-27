4d64672fadcc5690a0f47fbf46299320a5dac216
kerry <kerry@bluetech.com>
2014-10-27 17:26:09 +0800

    变量名修改

--- handlers/backchina.py
+++ handlers/backchina.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
-'''
-posting for backchina
-'''
+"""倍可亲回复模块
+
+@author: kerry
+@since: 2014-10-25
+@summary: 倍可亲论坛
+
+@var CHARSET: 倍可亲网页编码
+@type CHARSET: str
+"""
 from bs4 import BeautifulSoup
 from hashlib import md5
 from handlers.utils import get_datadic
@@ -13,29 +19,33 @@ from handlers.utils import RAPLogger
 # Captcha: arithmetic
 # Login: required
 def reply_backchina_forum(post_url, src):
-    '''
-        posting for backchina
-        parameters :
-                    post_url : 所需发帖论坛url
-                    src : 回复参数
-        return ：
-                返回是否登录成功，以及是否回帖成功
-        exception:
-                  无
-    '''
+    """倍可亲回复模块
+
+    @param sess:    requests.Session()
+    @type sess:     Session
+
+    @param post_url:   帖子地址
+    @type post_url:    str
+
+    @param src:        用户名，密码，回复内容，等等。
+    @type src:         dict
+
+    @return:           是否登录成功
+    @rtype:            bool
+
+    """
     # Returnable logger
     logger = RAPLogger(post_url)
 
     host = get_host(post_url)
-    session = RAPSession(src)
-    print session
+    sess = RAPSession(src)
 
     # Step 1: Login
     #登录页面
-    respond = session.get(
+    resp = sess.get(
         host + '/member.php?mod=logging&action=login&referer=')
     #获取登录页面content的HTML
-    soup = BeautifulSoup(respond.content)
+    soup = BeautifulSoup(resp.content)
     #查找与{'name': 'login'}匹配的form标签
     form = soup.find('form', attrs={'name': 'login'})
     #将form标签中的form属性内容存入payload中
@@ -46,11 +56,11 @@ def reply_backchina_forum(post_url, src):
     payload['password'] = md5(src['password']).hexdigest()
 
     #发送post包
-    respond = session.post(host + form['action'], data=payload)
-    soup = BeautifulSoup(respond.content)
+    resp = sess.post(host + form['action'], data=payload)
+    soup = BeautifulSoup(resp.content)
 
     #判断登录后页面是否含有用户字段，若存在则证明登录成功，否则失败
-    if src['username'] in respond.content:
+    if src['username'] in resp.content:
         logger.debug('Login OK')
     else:
         logger.error(
@@ -59,8 +69,8 @@ def reply_backchina_forum(post_url, src):
 
     # Step 2: Load post page
     # 获取所需发帖页面，查找与{'id':'fastpostform'}匹配的form标签
-    respond = session.get(post_url)
-    soup = BeautifulSoup(respond.content)
+    resp = sess.get(post_url)
+    soup = BeautifulSoup(resp.content)
     form = soup.find('form', attrs={'id':'fastpostform'})
 
     # Step 3: Submit
@@ -71,12 +81,12 @@ def reply_backchina_forum(post_url, src):
     payload['message'] = src['content']
 
     #发送post包
-    respond = session.post(host + form['action'], data=payload)
+    resp = sess.post(host + form['action'], data=payload)
     #获取回帖页面content的HTML
-    soup = BeautifulSoup(respond.content)
+    soup = BeautifulSoup(resp.content)
 
     #判断回帖后页面是否含有回帖内容，若存在则证明回帖成功，否则失败
-    if src['content'] in respond.content:
+    if src['content'] in resp.content:
         logger.debug('Reply OK')
     else:
         logger.error('Login Error: Reply Error, please try again !')
