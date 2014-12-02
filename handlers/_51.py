e9ed3639f232a05277e6b7e2ed6da8c605aca595
HSS <soongboyu@gmail.com>
2014-12-02 14:16:53 +0800

    51论坛发主帖

--- handlers/_51.py
+++ handlers/_51.py
@@ -5,15 +5,27 @@ from bs4 import BeautifulSoup
 
 import config
 from utils import *
+import utils
+from utils import get_datadic
 
 # Coding: utf8
 # Captcha: not required
 # Login: required
-def reply_51_forum(post_url, src):
-    logger = RAPLogger(post_url)
-    host = get_host(post_url)
-    s = RAPSession(src)
+def login_51_forum(post_url, s, src):
+    """51资讯论坛登录模块
 
+    @param sess:    requests.Session()
+    @type sess:     Session
+
+    @param src:        用户名，密码，回复内容，等等。
+    @type src:         dict
+
+    @return:           是否登录成功
+    @rtype:            bool
+
+    """
+    host = get_host(post_url)
+    
     r = s.get(host + 'logging.php?action=login')
     soup = BeautifulSoup(r.content)
     form = soup.find('form', attrs={'name': 'login'})
@@ -22,10 +34,22 @@ def reply_51_forum(post_url, src):
     payload['password'] = src['password']
     payload['fastloginfield'] = 'username'
     r = s.post(host + form['action'] + '&inajax=1', data=payload)
+    # 若指定字样出现在response中，表示登录成功
     if '欢迎您回来' not in r.content:
-        logger.error('Login Error')
+        return False
+    return True
+
+def reply_51_forum(post_url, src):
+    
+    logger = utils.RAPLogger(post_url)
+    host = utils.get_host(post_url)
+    s = utils.RAPSession(src)
+
+    # 登录
+    if not login_51_forum(post_url, s, src):
+        logger.error(' Login Error')
         return (False, str(logger))
-    logger.info('Login OK')
+    logger.info(' Login OK')
 
     r = s.get(post_url)
     soup = BeautifulSoup(r.content)
@@ -38,3 +62,58 @@ def reply_51_forum(post_url, src):
         return (False, str(logger))
     logger.info('Reply OK')
     return (True, str(logger))
+
+# Coding: utf8
+# Captcha: not required
+# Login: required
+def post_51_forum(post_url, src):
+    """51资讯论坛发主贴模块
+
+    @author: sky
+    @since: 2014-12-01
+
+    @param sess:    requests.Session()
+    @type sess:     Session
+
+    @param post_url:   帖子地址  http://bbs.51.ca/forumdisplay.php?fid=40
+    @type post_url:    str
+
+    @param src:        用户名，密码，回复内容，等等。
+    @type src:         dict
+
+    @return:           是否登录成功
+    @rtype:            bool
+
+    """
+    logger = utils.RAPLogger(post_url)
+    s = utils.RAPSession(src)
+
+    # 登录
+    if not login_51_forum(post_url, s, src):
+        logger.error(' Login Error')
+        return (False, str(logger))
+    logger.info(' Login OK')
+
+    fid = re.findall(r'fid=(\d*)', post_url)[0]
+    resp = s.get('http://bbs.51.ca/post.php?action=newthread&fid='+fid)
+    soup = BeautifulSoup(resp.content)
+    # 获得发帖form表单
+    form = soup.find('form', attrs={'id': 'postform'})
+
+    # 构造回复参数
+    payload = get_datadic(form)
+    payload['posttime'] = random.randint(1000000000,9999999999)
+    payload['wysiwyg'] = '1'
+    payload['subject'] = src['subject']
+    payload['message'] = src['content']
+
+    #发送发主贴post包
+    resp = s.post('http://bbs.51.ca/post.php?action=newthread&fid='+fid+'&extra=&topicsubmit=yes', data=payload)
+    # 若指定字样出现在response中，表示发帖成功
+    if src['subject'] not in resp.content:
+        logger.error(' Post Error')
+        return (False, '', str(logger))
+    logger.info(' Post OK')
+    url = resp.url
+    print url
+    return (True, url, str(logger))
