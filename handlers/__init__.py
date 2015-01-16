2a07c11d1a870bcce606e341ecd0f7dd8aab6768
sniper <sniper@bluetech.com>
2015-01-16 19:06:37 +0800

    Vanhi is ok now.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,6 +3,5 @@ import os
 
 # Import all the handlers
 if os.path.exists('handlers'):
-	for filename in os.listdir('handlers'):
-		if 'vanhi' not in filename:
-			exec('from %s import *' % filename.split('.')[0])
+    for filename in os.listdir('handlers'):
+        exec('from %s import *' % filename.split('.')[0])
