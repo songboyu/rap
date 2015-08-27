764ec20b88aeab1bbe94da9ecd6b016f34acb1a8
HSS <hss@bluetech.com>
2015-08-27 17:02:31 +0800

    selenium capture

--- tools/screen_print/screen_print_pyqt.py
+++ tools/screen_print/screen_print_pyqt.py
@@ -3,23 +3,24 @@
 Created on 2015年8月24日
 
 @author: yx
-要求3个参数，网址 保存名 搜索字符所存放文件名
+要求3个参数，网址 保存名 字符串数组
 '''
 import sys
 import os.path
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
  
 class Pagescreen(QWidget):
-    def __init__(self,url,savename,inputfile):
+    def __init__(self,url,savename,input=[]):
         QWidget.__init__(self)
         self.url = url
         self.savename = savename
         self.webpage = None
-        self.inputfile = inputfile
+        self.input = input
         fd = QFile("red.js")
         if fd.open(QIODevice.ReadOnly | QFile.Text):
             self.red = QTextStream(fd).readAll()
@@ -37,30 +38,32 @@ class Pagescreen(QWidget):
         web = QWebView(self)
         web.load(QUrl(self.url)) 
         self.webpage = web.page() 
-        self.connect(web,SIGNAL("loadFinished(bool)"),self.save)#事件连接
+        self.connect(web,SIGNAL("loadFinished(bool)"), self.save)#事件连接
 
     def input_str(self):
-        inputfile = open(self.inputfile,'r')
-        for line in inputfile.readlines():
+        for line in self.input:
             search_str = line.strip()
+
             code = u'redblock("'+search_str+'");'
             #code = u'var s="'+self.search_str+'";var reg=new RegExp("("+s+")","g");var str=document.body.innerHTML;var newstr=str.replace(reg,\'<span style="background:yellow">$1</span>\');document.body.innerHTML=newstr;null'
             self.webpage.mainFrame().evaluateJavaScript(code)
  
     def save(self,finished):
         if finished:
-            self.webpage.mainFrame().evaluateJavaScript(self.jquery)
-            self.webpage.mainFrame().evaluateJavaScript(self.red)
-            self.input_str()
+            # self.webpage.mainFrame().evaluateJavaScript(self.jquery)
+            # self.webpage.mainFrame().evaluateJavaScript(self.red)
+            # self.input_str()
             size = self.webpage.mainFrame().contentsSize()
             self.webpage.setViewportSize(QSize(size.width(),size.height()))
-            img = QImage(size, QImage.Format_ARGB32_Premultiplied)
+            img = QImage(size, QImage.Format_ARGB32)
+
             painter = QPainter(img)#绘图
             self.webpage.mainFrame().render(painter)
+            
             painter.end()
             img.save(self.savename)
         else:
-            print "载入错误，有可能网址错误！输入网址为" + self.url
+            print "载入错误，有可能网址错误！输入网址为 " + self.url
         self.close()
  
 def url_check(url):
@@ -69,11 +72,11 @@ def url_check(url):
 
 if __name__=="__main__":
     app = QApplication(sys.argv) 
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 3:
         if url_check(sys.argv[1]) == None:
             print "网址请输http:// or https://"
         else:
-            ps = Pagescreen(sys.argv[1], sys.argv[2], sys.argv[3])
+            ps = Pagescreen(sys.argv[1], sys.argv[2], [])
             ps.shot()
             sys.exit(app.exec_())#启动事件循环
     else:
---
+++ tools/screen_print/screen_print_selenium.py
@@ -0,0 +1,33 @@
+#-*-coding:utf-8-*-
+import time
+
+from selenium import webdriver
+from pyvirtualdisplay import Display
+from selenium.common.exceptions import TimeoutException
+
+class Pagescreen():
+    def __init__(self):
+        self.display = Display(visible=0, size=(1200, 900))
+        self.display.start()
+
+        self.driver = webdriver.Firefox() # Get local session of firefox
+        self.driver.set_window_size(800, 600)
+        self.driver.set_page_load_timeout(60)
+
+    def capture(self, url, save_fn="capture.png"):
+        try:
+            self.driver.get(url)
+            time.sleep(1)
+            self.driver.get_screenshot_as_file(save_fn)
+            print u'【save OK】: '+save_fn
+            print
+
+            return True
+        except TimeoutException:  
+            print 'time out after 30 seconds when loading page'
+            print  
+            self.driver.execute_script('window.stop()') #当页面加载时间超过设定时间，通过执行Javascript来stop加载，即可执行后续动作
+            return False
+
+    def quit():
+        self.driver.quit()
